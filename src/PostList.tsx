c30d383e8d940b454776d46f7b3999ae598328f9
Jeremelbaz <jerem.elbaz@gmail.com>
2025-02-12 19:09:17 +0200

    added all the registration, i also begin the services

---
+++ src/PostList.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import Post from "./components/Post";
+import axios from "axios";
+
+// Updated PostData interface with new properties
+interface PostData {
+    title: string;
+    content: string;
+    owner: string; // Reference to user
+    image?: string; // Optional image path
+    likes: string[]; // Array of user IDs who liked the post
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+function PostsList() {
+    const [posts, setPosts] = useState<PostData[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        console.log("Fetching posts...");
+        axios
+            .get<PostData[]>("http://localhost:3000")
+            .then((response) => {
+                console.log("Posts received:", response.data);
+                setPosts(response.data);
+            })
+            .catch((err) => {
+                console.error("Error fetching posts:", err);
+                setError("Failed to load posts.");
+            })
+            .finally(() => setLoading(false));
+
+        return () => {
+            console.log("Cleanup function executed");
+        };
+    }, []);
+
+    if (loading) return <p>Loading posts...</p>;
+    if (error) return <p style={{ color: "red" }}>{error}</p>;
+
+    return (
+        <div>
+            {posts.map((post) => (
+                <Post key={post.updatedAt.toString()} post={post} />
+            ))}
+        </div>
+    );
+}
+
+export default PostsList;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import 'bootstrap/dist/css/bootstrap.css'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <GoogleOAuthProvider clientId="764938356434-9sg7heli6dv8nj6rlobhhjlq18ui5dpr.apps.googleusercontent.com">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 )
